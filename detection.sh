46343c05d1e9845cc1ffe87fc9ec46b32371421a
ji-hun-choi <chlwlgns620@naver.com>
2024-06-20 17:02:53 +0900

    Modify path

--- detection.sh
+++ detection.sh
@@ -4,11 +4,15 @@ PASSWORD="root1234"
 echo $PASSWORD | sudo -kS pkill -9 python3*
 
 PYTHON=/usr/bin/python3
-AI=/home/iist
+#AI=/home/iist
+AI=/usr/src/ultralytics/new_AI_box
+
+sleep 1;
+#echo $PASSWORD | sudo -kS ${PYTHON} $AI/main.py&
+${PYTHON} $AI/main.py&
 
 
 sleep 1;
-echo $PASSWORD | sudo -kS ${PYTHON} $AI/main.py&
+#echo $PASSWORD | sudo -kS ${PYTHON} $AI/web.py
+${PYTHON} $AI/web.py
 
-sleep 1;
-echo $PASSWORD | sudo -kS ${PYTHON} $AI/web.py
