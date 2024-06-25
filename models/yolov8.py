beea0d711dddacd4fbb962cc58ab8b7cc773a0c6
ji-hun-choi <chlwlgns620@naver.com>
2024-06-25 11:20:23 +0900

    update

--- models/yolov8.py
+++ models/yolov8.py
@@ -7,7 +7,7 @@ class YOLOv8:
     def __init__(self, name, weight_path, conf_thr=.1, iou_thr=.05, persist=True, verbose=False,
                  tracker="./files/cfg/bytetrack.yaml"):
         self.name = name
-        self.model = ultralytics.YOLO(weight_path)
+        self.model = ultralytics.YOLO(weight_path, task='detect')
         self.img_size = 640
         self.conf_thr = conf_thr
         self.iou_thr = iou_thr
